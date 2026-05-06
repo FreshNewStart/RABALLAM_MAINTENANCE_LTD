21eb8ea4b270eaa420659b3ac820b3757d657066
FreshNewStart <mike.aswad.j@gmail.com>
2026-05-06 19:07:48 +1000

    Astro Pages Configuration

--- astro-version/astro.config.mjs
+++ astro-version/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+
+  site: 'https://FreshNewStart.github.io',
+
+  base: '/RABALLAM_MAINTENANCE_LTD/',
+});
